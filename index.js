ae064889b7881ba9640aa68303a2d08c456930c3
Thomas Noel <thomas.c.noel@gmail.com>
2023-11-13 12:12:49 -0800

    Added password validation; finished v1

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// TODO: Put stuff in here
+
+// Grab the form and disable default validation
+const form = document.getElementById("user-info-form");
+form.noValidate = true;
+
+const passwordsValid = (pwdA, pwdB) => {
+  return pwdA === pwdB;
+}
+
+form.addEventListener("submit", (e) => {
+  const formElement = e.target;
+
+  const pwdA = document.querySelector('#user-password').value;
+  const pwdB = document.querySelector('#user-confirm-password').value;
+  const passwordsMatch = passwordsValid(pwdA, pwdB);
+
+  const userPwd = document.getElementById("user-password");
+  const userConfirmPwd = document.getElementById("user-confirm-password"); 
+  const errorElem = document.getElementById("error");
+
+  const borderColor = "#E5E7EB";
+  const errorBorderColor = "#D97E7B";
+ 
+  if (formElement.checkValidity() && passwordsMatch) {
+  } else {
+    if (!passwordsMatch) {
+      userPwd.style.borderColor = errorBorderColor;
+      userConfirmPwd.style.borderColor = errorBorderColor;
+
+      errorElem.style.display = "block"; 
+
+      console.log(pwdA);
+      console.log(pwdB);
+    } else {
+      if (userPwd.classList.contains("invalid")) {
+        userPwd.style.borderColor = borderColor;
+        userConfirmPwd.style.borderColor = borderColor;
+          
+        errorElem.style.display = "none";
+      }
+    }
+
+    // Stop the page from submitting and refreshing
+    e.preventDefault();
+  }
+});
